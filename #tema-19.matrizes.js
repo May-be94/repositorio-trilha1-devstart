e870670cefe8adc70b56f0480c5c60269782bfda
Mayra Cardoso <110548147+May-be94@users.noreply.github.com>
2023-03-14 11:22:08 -0300

    Create #tema-19.matrizes.js

---
+++ #tema-19.matrizes.js
@@ -0,0 +1,22 @@
+/*matrizes  são estruturas de dados 
+uma matriz é uma lista de valores 
+em vez de definir cinco variaveis chamadas nota1,nota2...Voce pode
+definir i,a variavel chamada notas, que é uma matriz contendo esses numeros.
+o funcionamento da matriz é semelhante a uma planilha no excel ou google docs*/
+
+//MATRIZ VAZIA: let notas = []
+
+/* para acessar um item de uma matriz, você deve escrever o nome da matriz, seguido pelo
+indice do item entre colchetes []. 
+O indice é o numero que representa a posição do item dentro da matriz, e 
+começa com 0*/
+
+let notas = [10, 5, 13]
+let primeiraNota = notas[0]
+
+console.log(primeiraNota)
+
+let temperaturas = [-5, 12, 15, 18, 13]
+let valor = temperaturas[2]
+
+console.log(valor)
